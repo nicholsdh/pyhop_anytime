52e6ee85213a3784042bfaf1d29b87aa5061b4d8
gjf2a <ferrer@hendrix.edu>
2022-02-05 00:42:04 -0600

    Added Oset, which will avoid the problems of the nondeterministic set() implementation.

--- pyhop_anytime/__init__.py
+++ pyhop_anytime/__init__.py
@@ -1,1 +1,2 @@
 from pyhop_anytime.pyhop import *
+from pyhop_anytime.oset import *
---
+++ pyhop_anytime/oset.py
@@ -0,0 +1,45 @@
+import unittest
+
+
+class Oset:
+    def __init__(self, items=None):
+        self.items = {}
+        if items:
+            for item in items:
+                self.add(item)
+
+    def __eq__(self, other):
+        return self.items == other.items
+
+    def __repr__(self):
+        return f'Oset({[item for item in self.items]})'
+
+    def __contains__(self, item):
+        return item in self.items
+
+    def add(self, item):
+        self.items[item] = None
+
+    def discard(self, item):
+        if item in self.items:
+            del self.items[item]
+
+    def __iter__(self):
+        return self.items.__iter__()
+
+
+class Test(unittest.TestCase):
+    def test1(self):
+        s = Oset()
+        for i in range(10):
+            s.add(i // 2)
+        print(s)
+        self.assertEqual(s, Oset([0, 1, 2, 3, 4]))
+        self.assertEqual(s, Oset([4, 3, 2, 1, 0]))
+
+        for i, item in enumerate(s):
+            self.assertEqual(i, item)
+
+
+if __name__ == '__main__':
+    unittest.main()
