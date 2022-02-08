be229a83d3e62a1d7dff0b6d0e5b35cea52d97d4
gjf2a <ferrer@hendrix.edu>
2022-02-07 18:06:28 -0600

    Significantly tweaked anyhop_main().

--- pyhop_anytime/auxiliary.py
+++ pyhop_anytime/auxiliary.py
@@ -13,7 +13,8 @@ def anyhop_main(planner):
             elif filename.startswith("-s"):
                 max_seconds = float(filename.split(':')[1])
             else:
-                exec(f"from {filename} import *")
+                exec(f"from {filename} import state_goals")
+                state, goals = state_goals()
                 plans = planner.anyhop(state, [('start', goals)], max_seconds=max_seconds, verbose=verbosity)
                 for (plan, time) in plans:
                     print(plan)
